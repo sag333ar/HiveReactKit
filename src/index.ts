a92937ba70b883f5c467728f3bfe210190e29e3f
Lokesh Burade <lokeshburade007@gmail.com>
2026-02-24 18:22:17 +0530

    version bump comment modal added

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ export { default as ActivityList } from './components/ActivityList';
 export { default as ListOfWitnesses } from './components/ListOfWitnesses';
 export { default as TransactionHistory } from './components/TransactionHistory';
 export { default as UserChannel } from './components/UserChannel';
+export { default as CommentsModal } from './components/comments/CommentsModal';
 
 // Common components
 export { default as FavouriteWidget } from './components/common/FavouriteWidget';
